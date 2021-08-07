a79ef2b6e80f1f16c93c8b2dd854ceb762480291
Istiackrohan <istiackrohan@gmail.com>
2021-08-08 01:17:47 +0600

    Sidebar e, Navbar e box-shaddow add korsi, image ta add korsi

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -6,6 +6,7 @@ import { IconContext } from 'react-icons/lib';
 
 const Nav = styled.div`
     background-image: linear-gradient(rgb(41, 41, 41), rgb(83, 27, 27), rgb(122, 33, 33));
+    box-shadow: 10px 0px 20px black;
     height: 80px;
     display: flex;
     justify-content: flex-start;
@@ -14,11 +15,12 @@ const Nav = styled.div`
 
 const SidebarNav = styled.nav`
     background: #15171c;
-    width: 300px;
+    width: 275px;
     height: 100vh;
     display: flex;
     justify-content: center;
     position: fixed;
+    box-shadow: 0px 10px 20px black;
     top: 0;;
     transition: 350ms;
     z-index: 10;
@@ -37,7 +39,7 @@ const Sidebar = () => {
                 </Nav>
                 <SidebarNav>
                     <SidebarWrap>
-                        {<img style={{margin: "50px"}} src="10ms-contents\images\logo.png"></img>}
+                        {<img style={{marginTop: "80px", marginBottom: "50px", marginLeft: "80px", marginRight: "80px",}} src="https://github.com/Istiackrohan/10ms-contents/blob/main/images/logo.png?raw=true" alt="Logo"></img>}
                         {SidebarData.map((item, index) => {
                             return <SubMenu item={item} key={index} />;
                         })}
